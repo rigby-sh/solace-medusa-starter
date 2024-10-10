c65a4d5c29984714cb7c1931485341d7d254509d
agnwoj99 <myvogue99@wp.pl>
2024-10-10 16:39:57 +0200

    [AC-193-194] Created Latest orders section and current orders

--- src/modules/account/templates/account-layout.tsx
+++ src/modules/account/templates/account-layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { HttpTypes } from '@medusajs/types'
-import UnderlineLink from '@modules/common/components/interactive-link'
 
 import AccountNav from '../components/account-nav'
 
@@ -20,20 +19,6 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
         <div className="grid grid-cols-1 py-12 small:grid-cols-[240px_1fr]">
           <div>{customer && <AccountNav customer={customer} />}</div>
           <div className="flex-1">{children}</div>
-        </div>
-        <div className="flex flex-col items-end justify-between gap-8 border-gray-200 py-12 small:flex-row small:border-t">
-          <div>
-            <h3 className="text-xl-semi mb-4">Got questions?</h3>
-            <span className="txt-medium">
-              You can find frequently asked questions and answers on our
-              customer service page.
-            </span>
-          </div>
-          <div>
-            <UnderlineLink href="/customer-service">
-              Customer Service
-            </UnderlineLink>
-          </div>
         </div>
       </div>
     </div>
